72737019ac85779e0916b0178d60cc410832c1c4
Oksanka848 <okmitka@mail.ru>
2022-10-11 10:28:10 +0000

    ch

--- js14.js
+++ js14.js
@@ -1,5 +1,6 @@
 /*Перепишите домашнее задание прошлой недели c помощью 
-input для ввода данных и div (либо input) для вывода результата*/
+input для ввода данных и div (либо input) для вывода результата
+2.Напишите скрипт, который меняет фон div-ов по клику на них*/
 /*window.onload = () => {
     init();
   };
@@ -37,6 +38,15 @@ function ButtonClick4() {
   document.getElementById("result").innerHTML = (+n1 / +n2);
 
 }
+
+function changeColor () {
+  let div = document.getElementsByTagName('div')
+  console.log('Clicked on button', div.style)
+  div.style.color = 'red'
+  div.style.fontSize = '24px'
+}
+
+let divs = document.getElementsByTagName('div');
     /*alert(`Результат вычисления = ${+n1 - +n2}`);
   }*/
   
