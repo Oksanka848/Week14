c6881cd30c9aad111179fdc822d3b21ad52c1d8b
Oksanka848 <okmitka@mail.ru>
2022-10-13 09:44:26 +0000

    ch

--- gallary/gallary.js
+++ gallary/gallary.js
@@ -2,28 +2,11 @@
 Вперед и Назад (она не должна работать идеально, 
     это пока просто заготовка на будущее!)*/
 
-var i = 0;
-var image = document.getElementById("mySlides");
-var imgs = new Array(
-  "img/item1.jpg",
-  "img/item2.jpeg",
-  "img/item3.jpg",
-  "img/item4.jpeg"
-);
-var j = imgs;
-
-function imgsrc() {
-  if (i != j + 1) {
-    i++;
-  }
-  image.src = imgs[i];
-  slides[i].style.display = "none";
-}
-function imgsrc2() {
-  if (i != j - 1) {
-    i--;
-  }
-
-  image.src = imgs[i];
-  slides[i].style.display = "none";
-}
+var i=0;
+                var image = document.getElementById("myImages");
+                var imgs = ['img/item1.jpg','img/item2.jpeg','img/item3.jpg','img/item4.jpeg','img/item5.jpg'];
+                function imgsrc() {
+                    i++;
+                    i %= imgs.length;
+                    image.src = imgs[i];
+                }
