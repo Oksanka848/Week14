d2e10ee62388b0adaa673f5bd0045235d2806cbd
Oksanka848 <okmitka@mail.ru>
2022-10-13 09:59:54 +0000

    ch

--- gallary/gallary.js
+++ gallary/gallary.js
@@ -5,8 +5,14 @@
 var i=0;
                 var image = document.getElementById("myImages");
                 var imgs = ['img/item1.jpg','img/item2.jpeg','img/item3.jpg','img/item4.jpeg','img/item5.jpg'];
+                var imgs2 = ['img/item5.jpg','img/item4.jpeg','img/item3.jpg','img/item2.jpeg','img/item1.jpg'];
                 function imgsrc() {
                     i++;
                     i %= imgs.length;
                     image.src = imgs[i];
                 }
+                function imgsrc2() {
+                  i++;
+                  i %= imgs2.length;
+                  image.src = imgs2[i];
+              }
